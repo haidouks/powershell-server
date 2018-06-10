d92f2f17bed590bd7718a0c8c8c957b11ee6af25
Cansın Aldanmaz <cansinaldanmaz@Cansns-MacBook-Pro.local>
2018-06-10 19:25:28 +0300

    comments removed

--- assets/powershell-server.ps1
+++ assets/powershell-server.ps1
@@ -8,69 +8,36 @@ $req = $null
  
 while ($true) {
     $context = $listener.GetContext() 
- 
     $request = $context.Request
- 
-    $response = $context.Response
-   
-    if ($request.Url -match '/end$') { 
-        break 
-    }
-    else {
-        $requestvars = ([String]$request.Url).split("/");
-        Write-Verbose -Message "Received request: $($request.Url)"
-        Write-Verbose -Message "Request Details:`n $($request | fl * -force | Out-String -Stream)"
-        <#
-        $folders = (Get-ChildItem -Path $PSScriptRoot -Directory | Select-Object BaseName).BaseName
-        $application = @{}
-        $application.exists = $false
-        $application.name = $requestvars[3]
-        $application.subname = $requestvars[4].split("?")[0]
-        Write-Verbose -Message "Looking for application $($application.Name)/$($application.subname)"
-        Write-Verbose -Message "Current application list:$folders"
-        foreach($folder in $folders)
-        {
-            if ($application.name -eq $folder)
-            {
-                $subfolders = (Get-ChildItem -Path $(Join-Path -Path $PSScriptRoot -ChildPath $application.name) -Directory | Select-Object BaseName).BaseName
-                foreach($subfolder in $subfolders)
-                {
-                    if ($application.subname -eq $subfolder)
-                    {
-                        Write-Verbose -Message "Found $($application.name)\$($application.subname)"
-                        $application.exists = $true
-                        break;
-                    }
-                }
-            }
-        }
-        if ($application.exists) {
-        #>
-        $message = "Welcome to Powershell Server"
-        $body = $request.InputStream
-        $QueryStrings = New-Object System.Collections.ArrayList
-        foreach($queryKey in $request.QueryString.Keys) {
+    $response = $context.Response   
+    $requestvars = ([String]$request.Url).split("/");
+    Write-Verbose -Message "Received request: $($request.Url)"
+    Write-Verbose -Message "Request Details:`n $($request | fl * -force | Out-String -Stream)"
+    $message = "Welcome to Powershell Server"
+    $body = $request.InputStream
+    $QueryStrings = New-Object System.Collections.ArrayList
+    foreach($queryKey in $request.QueryString.Keys) {
             Write-Verbose -Message "Adding queryString $queryKey --> $($request.QueryString.GetValues($queryKey))"
             $queryString = @{
                 $queryKey = $($request.QueryString.GetValues($queryKey))
             }
             $null=$QueryStrings.Add($queryString)
         }
-        $parameters = @{
+    $parameters = @{
             queryStrings = $QueryStrings
             body = $body
         }
-        try {
+    try {
             Write-Verbose -Message "Executing $PSScriptRoot\$($application.name)\$($application.subname)\index.ps1"
             $result = & "$PSScriptRoot\$($requestvars[3])\$($requestvars[4])\index.ps1" @parameters 
             $message = $result.output | Out-String
             $response.ContentType = $result.contentType;
         }
-        catch {
+    catch {
             $message += "<br/>Details<br/>$_.Exception";
             $response.ContentType = 'text/html' ;
         }
-        finally { 
+    finally { 
             [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
             $response.ContentLength64 = $buffer.length
             $output = $response.OutputStream
@@ -79,6 +46,5 @@ while ($true) {
             $response.Close()
             #Free memory
         }
-   }    
-}
+    }
 $listener.Stop()
