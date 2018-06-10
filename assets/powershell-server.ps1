5f5a4db92969f56cbef53bcfc582bbb85bac9f23
Cansın Aldanmaz <canald@gmail.com>
2018-06-10 23:37:04 +0300

    Update powershell-server.ps1

--- assets/powershell-server.ps1
+++ assets/powershell-server.ps1
@@ -7,43 +7,42 @@ write-host "Listening On Port: $port"
 $req = $null
  
 while ($true) {
-    $context = $listener.GetContext() 
-    $request = $context.Request
-    $response = $context.Response   
-    $requestvars = ([String]$request.Url).split("/");
-    Write-Verbose -Message "Received request: $($request.Url)"
-    Write-Verbose -Message "Request Details:`n $($request | fl * -force | Out-String -Stream)"
+    $context = $listener.GetContext()
+    $requestvars = ([String]$context.Request.Url).split("/");
+    Write-Verbose -Message "Received request: $($context.Request.Url)"
+    Write-Verbose -Message "Request Details:`n $($context.Request | fl * -force | Out-String -Stream)"
     $message = "Welcome to Powershell Server"
-    $body = $request.InputStream
+    $body = $context.Request.InputStream
     $QueryStrings = New-Object System.Collections.ArrayList
-    foreach($queryKey in $request.QueryString.Keys) {
-            Write-Verbose -Message "Adding queryString $queryKey --> $($request.QueryString.GetValues($queryKey))"
+    
+    try {
+      foreach($queryKey in $context.Request.QueryString.Keys) {
+            Write-Verbose -Message "Adding queryString $queryKey --> $($context.Request.QueryString.GetValues($queryKey))"
             $queryString = @{
-                $queryKey = $($request.QueryString.GetValues($queryKey))
+                $queryKey = $($context.Request.QueryString.GetValues($queryKey))
             }
             $null=$QueryStrings.Add($queryString)
         }
-    $parameters = @{
+      $parameters = @{
             queryStrings = $QueryStrings
             body = $body
         }
-    try {
             Write-Verbose -Message "Executing $PSScriptRoot\$($application.name)\$($application.subname)\index.ps1"
             $result = & "$PSScriptRoot\$($requestvars[3])\$($requestvars[4].split("?")[0])\index.ps1" @parameters 
             $message = $result.output | Out-String
-            $response.ContentType = $result.contentType;
+            $context.Response.ContentType = $result.contentType;
         }
     catch {
             $message += "<br/>Details<br/>$_.Exception";
-            $response.ContentType = 'text/html' ;
+            $context.Response.ContentType = 'text/html' ;
         }
     finally { 
             [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
-            $response.ContentLength64 = $buffer.length
-            $output = $response.OutputStream
+            $context.Response.ContentLength64 = $buffer.length
+            $output = $context.Response.OutputStream
             $output.Write($buffer, 0, $buffer.length)
             $output.Close()
-            $response.Close()
+            $context.Response.Close()
             #Free memory
         }
     }
