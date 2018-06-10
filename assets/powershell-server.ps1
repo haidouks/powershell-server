9265b79d25dc477a1b4c7b93170ba432b97a8700
Cansın Aldanmaz <canald@gmail.com>
2018-06-10 19:12:46 +0300

    Update powershell-server.ps1
    Performance optimization

--- assets/powershell-server.ps1
+++ assets/powershell-server.ps1
@@ -20,6 +20,7 @@ while ($true) {
         $requestvars = ([String]$request.Url).split("/");
         Write-Verbose -Message "Received request: $($request.Url)"
         Write-Verbose -Message "Request Details:`n $($request | fl * -force | Out-String -Stream)"
+        <#
         $folders = (Get-ChildItem -Path $PSScriptRoot -Directory | Select-Object BaseName).BaseName
         $application = @{}
         $application.exists = $false
@@ -44,36 +45,40 @@ while ($true) {
             }
         }
         if ($application.exists) {
-            $body = $request.InputStream
-            $QueryStrings = New-Object System.Collections.ArrayList
-            foreach($queryKey in $request.QueryString.Keys) {
-                Write-Verbose -Message "Adding queryString $queryKey --> $($request.QueryString.GetValues($queryKey))"
-                $queryString = @{
-                    $queryKey = $($request.QueryString.GetValues($queryKey))
-                }
-                $null=$QueryStrings.Add($queryString)
+        #>
+        $message = "Welcome to Powershell Server"
+        $body = $request.InputStream
+        $QueryStrings = New-Object System.Collections.ArrayList
+        foreach($queryKey in $request.QueryString.Keys) {
+            Write-Verbose -Message "Adding queryString $queryKey --> $($request.QueryString.GetValues($queryKey))"
+            $queryString = @{
+                $queryKey = $($request.QueryString.GetValues($queryKey))
             }
-            $parameters = @{
-                queryStrings = $QueryStrings
-                body = $body
-            }
+            $null=$QueryStrings.Add($queryString)
+        }
+        $parameters = @{
+            queryStrings = $QueryStrings
+            body = $body
+        }
+        try {
             Write-Verbose -Message "Executing $PSScriptRoot\$($application.name)\$($application.subname)\index.ps1"
-            $result = & "$PSScriptRoot\$($application.name)\$($application.subname)\index.ps1" @parameters 
+            $result = & "$PSScriptRoot\$($requestvars[3])\$($requestvars[4])\index.ps1" @parameters 
             $message = $result.output | Out-String
             $response.ContentType = $result.contentType;
-        } 
-        else {
-            $message = "Welcome to Powershell Server";
+        }
+        catch {
+            $message += "<br/>Details<br/>$_.Exception";
             $response.ContentType = 'text/html' ;
-       }
-        
-       [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
-       $response.ContentLength64 = $buffer.length
-       $output = $response.OutputStream
-       $output.Write($buffer, 0, $buffer.length)
-       $output.Close()
-       #Free memory
-       Clear-variable -Name "message","output","body","result","response","request","context","parameters"
+        }
+        finally { 
+            [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
+            $response.ContentLength64 = $buffer.length
+            $output = $response.OutputStream
+            $output.Write($buffer, 0, $buffer.length)
+            $output.Close()
+            $response.Close()
+            #Free memory
+        }
    }    
 }
 $listener.Stop()
